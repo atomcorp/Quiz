e54abcd13c3561b2a60e737df414fb2a459b9acd
atomcorp <thomasmaxwellsmith@gmail.com>
2016-10-29 17:49:04 +0100

    gets the q&a, next button gets next question

--- quiz2.js
+++ quiz2.js
@@ -15,18 +15,19 @@ var quizJSON = {
 		{
 			quizStage: 1,
 			question: 'What is 1 + 1?',
+			// answers should always be strings?
 			answers: [
 				{
-					answer: 1,
+					answer: '1',
 					isCorrect: false
 				}, {
-					answer: 2,
+					answer: '2',
 					isCorrect: true
 				}, {
-					answer: 3,
+					answer: '3',
 					isCorrect: false
 				}, {
-					answer: 4,
+					answer: '4',
 					isCorrect: false
 				}
 			],
@@ -71,53 +72,130 @@ var Score = function() {
 var TakeQuiz = function() {
 	// give TakeQuiz a quizJSON
 
-	var quiz, currentStage;
-
 	var settings = {
 		maxQuizStages: 10,
 		minQuizStages: 1,
-		currentQuizStage: 1,
+		length: 0,
+	}
 
+	var currentStage = {
+		quiz: {},
+		currentQuestion: '',
+		currentCorrectAnswer: '',
+		points: 0,
+		position: 1,
+	}
+
+	var elSetup = {
+		nextButton: function() {
+			var nextButton = document.getElementById('next');
+			nextButton.addEventListener('click', _nextQuestion, false);
+		}
+	}
+
+	// http://stackoverflow.com/questions/18534314/reuse-elements-of-html
+	var answerHtml = {
+		config: {
+			text: '',
+			id: ''
+		},
+
+		init: function() {
+			var answerContainer = document.createElement('div');
+			answerContainer.className = 'answer';
+			answerContainer.dataset.update = 'answer';
+			answerContainer.dataset.id = this.config.id;
+			answerContainer.textContent = this.config.text;
+			answerContainer.addEventListener('click', _checkIfAnswerCorrect, false);
+			return answerContainer;
+		}
 	}
 
 	var init = function(id) {
 		_setQuiz(id);
 	}
 
+	var _checkIfAnswerCorrect = function() {
+		if (this.textContent === currentStage.currentCorrectAnswer) {
+			// Add the points one to the total points
+			settings.points += currentStage.points;
+		} else {
+			console.log('No');
+		}
+	}
+
 	var _setQuiz = function() {
 		// is given quiz ID
-		// would make a call to the databse and grab the correct quiz 
+		// would make a call to the database and grab the correct quiz 
 
 		// for testing there's only one quiz to take 
-		quiz = quizJSON;
+		currentStage.quiz = quizJSON;
+		settings.length = currentStage.quiz.length;
+		elSetup.nextButton();
 	}
 
 	var _setQuizStage = function() {
-
+		var thisStage = currentStage.quiz.quiz;
 		// check the quiz stage and get the correct question
-		for (var i = 0; i < quiz.quiz.length; i++) {
-			if (settings.currentQuizStage = quiz.quiz[i]) {
-				currentStage = quiz.quiz[i];
+		for (var i = 0; i < thisStage.length; i++) {
+			if (currentStage.position === thisStage[i].quizStage) {
+				currentStage.currentQuestion = thisStage[i];
+				currentStage.currentCorrectAnswer = _getCorrectAnswer(thisStage[i].answers);
+				currentStage.points = thisStage[i].points;
+				return;
+			}
+		}
+
+	}
+
+	var _getCorrectAnswer = function(answers) {
+		console.log(answers);
+		for (var i = 0; i < answers.length; i++) {
+			if (answers[i].isCorrect === true) {
+				return answers[i].answer;
 			}
 		}
 	}
 
 	var _paintQuizStage = function() {
-		var domQuestion = document.querySelectorAll('[data-update="question"]');
-		var domAnswers = document.querySelectorAll('.answers');
-		var domAnswer = document.querySelectorAll('[data-update="answer"]');
-		domQuestion[0].textContent = currentStage.question;
-		
-		console.log(currentStage.answers.length);
-
+		var domAnswersContainer = document.querySelectorAll('.answers');
+		var thisQuestion = currentStage.currentQuestion;
 		// how many answers are there
-		if (currentStage.answers.length > 1) {
-			for (var i = 0; i < currentStage.answers.length; i++) {
-				var createdAnswer = domAnswer[0].cloneNode(false);
-				createdAnswer.textContent = currentStage.answers[i].answer;
-				domAnswers[0].appendChild(createdAnswer);
+		console.log(thisQuestion);
+		if (thisQuestion.answers.length > 1) {
+			for (var i = 0; i < thisQuestion.answers.length; i++) {
+				// var createdAnswer = domAnswer[0].cloneNode(false);
+				// createdAnswer.textContent = currentQuestion.answers[i].answer;
+				// domAnswers[0].appendChild(createdAnswer);
+				var addAnswer = Object.create(answerHtml);
+				addAnswer.config.text = thisQuestion.answers[i].answer;
+				addAnswer.config.id = i + 1;
+				var addedAnswer = domAnswersContainer[0].appendChild(addAnswer.init());
+
 			}
 		}
+	}
+
+	var _nextQuestion = function() {
+		// get current stage,
+		// is current stage + 1 more than quiz stages allowed?
+		console.log(currentStage.position);
+		if (currentStage.position === settings.length) {
+			console.log('Stop quiz');
+		} else {
+			currentStage.position += 1;
+			_setQuizStage();
+			_paintQuizStage();
+		}
+	}
+
+	var _manageQuizState = function() {
+		// get how many quiz stages
+		// get current quiz stage 
+
+		// next question el
+
+		// start quiz
 	}
 
 	var showQuiz = function() {
